66b01e577e66bca1f4393e53ccadd49284e44584
Justin Beall <jus.beall@gmail.com>
2016-02-28 00:10:59 -0500

    Add Tim McGraw lyrics

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from distutils.core import setup
+
 setup(
-  name = 'tay_say',
-  packages = ['tay_say'], # this must be the same as the name above
-  version = '0.1',
-  description = 'A simple lib that prints out a random Taylor Swift lyric',
-  author = 'Justin Beall',
-  author_email = 'jus.beall@gmail.com',
-  url = 'https://github.com/DEV3L/python-tay-say',
-  download_url = 'https://github.com/DEV3L/python-tay-say/tarball/0.1',
-  keywords = ['testing', 'dev3l', 'taylor swift'], # arbitrary keywords
+    name='tay_say',
+    packages=['tay_say'],  # this must be the same as the name above
+    version='0.3',
+    description='A simple lib that prints out a random Taylor Swift lyric',
+    author='Justin Beall',
+    author_email='jus.beall@gmail.com',
+    url='https://github.com/DEV3L/python-tay-say',
+    download_url='https://github.com/DEV3L/python-tay-say/tarball/0.3',
+    keywords=['testing', 'dev3l', 'taylor swift'],  # arbitrary keywords
     classifiers=[
         'Environment :: Console',
         'Intended Audience :: Developers',
