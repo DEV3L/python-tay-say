701cd37d396dc690f4a0d6dc45390f5842773c45
Justin Beall <jus.beall@gmail.com>
2016-02-27 23:16:23 -0500

    Create test output

--- setup.py
+++ setup.py
@@ -9,5 +9,17 @@ setup(
   url = 'https://github.com/DEV3L/python-tay-say',
   download_url = 'https://github.com/DEV3L/python-tay-say/tarball/0.1',
   keywords = ['testing', 'dev3l', 'taylor swift'], # arbitrary keywords
-  classifiers = [],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+    entry_points={
+        'console_scripts': [
+            'tay_say = tay_say:print_lyric'
+        ]},
 )
